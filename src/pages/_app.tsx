1c2443b9ff8c4ba379471bfe2a528271705972bc
prathamrpatel <rprathampatel@gmail.com>
2023-12-01 18:27:34 -0500

    Setup planet scale

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
-import { type AppType } from "next/app";
-
-import { api } from "~/utils/api";
-
-import "~/styles/globals.css";
+import { type AppType } from 'next/app';
+import { api } from '~/utils/api';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
