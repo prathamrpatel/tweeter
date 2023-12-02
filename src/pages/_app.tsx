55f6a774d1b408ad80b000c0357660843bf6f5cb
prathamrpatel <rprathampatel@gmail.com>
2023-12-01 19:20:46 -0500

    Added user auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { type AppType } from 'next/app';
 import { api } from '~/utils/api';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ClerkProvider>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ClerkProvider>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,17 @@
-import Head from 'next/head';
-import Link from 'next/link';
 import { api } from '~/utils/api';
-import styles from './index.module.css';
 import { Heading } from '@chakra-ui/react';
+import { UserButton } from '@clerk/nextjs';
+import NavBar from '~/components/NavBar';
 
 export default function Home() {
   const hello = api.post.hello.useQuery({ text: 'from tRPC' });
 
   return (
     <>
-      <div>
-        <div>welcome to tweeter</div>
-        <Heading>hello</Heading>
-      </div>
+      <NavBar />
+      <div>welcome to tweeter</div>
+      <Heading>hello</Heading>
+      <UserButton afterSignOutUrl="/" />
     </>
   );
 }
